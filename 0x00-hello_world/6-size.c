0f6185b264d93d199cbbe709150cacf9764b913b
= <=paulmanifest@gmail.com>
2022-04-07 09:59:00 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%i byte(s)",sizeof(char));
+printf("%i byte(s)",sizeof(int));
+printf("%i byte(s)",sizeof(Long int));
+printf("%i byte(s)",sizeof(Long Long int));
+printf("%i byte(s)",sizeof(float);
+return (0);
+}
