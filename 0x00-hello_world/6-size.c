eaeb79653a03e4e61c5760f738307c51f91b431c
= <=paulmanifest@gmail.com>
2022-04-08 04:36:44 -0700

    tak 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,14 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("%i byte(s)",sizeof(char));
-printf("%i byte(s)",sizeof(int));
-printf("%i byte(s)",sizeof(Long int));
-printf("%i byte(s)",sizeof(Long Long int));
-printf("%i byte(s)",sizeof(float);
-return (0);
+	printf("%i byte(s)",sizeof(char));
+	printf("%i byte(s)",sizeof(int));
+	printf("%i byte(s)",sizeof(Long int));
+	printf("%i byte(s)",sizeof(Long Long int));
+	printf("%i byte(s)",sizeof(float);
+	return (0);
 }
