1ece13ef2c293ea50e88374ee93f16fd46cef83c
= <=paulmanifest@gmail.com>
2022-04-08 06:08:57 -0700

    well

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("%li byte(s)",sizeof(char));
-	printf("%li byte(s)",sizeof(int));
-	printf("%li byte(s)",sizeof(Long int));
-	printf("%li byte(s)",sizeof(Long Long int));
-	printf("%i byte(s)",sizeof(float);
+	printf("Size of a char: %zu byte(s)\n",sizeof(char));
+	printf("Size of an int: %zu byte(s)\n",sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n",sizeof(Long int));
+	printf("Size of a long long int: %zu byte(s)\n",sizeof(Long Long int));
+	printf("Size of a long long int: %zu byte(s)\n",sizeof(float);
 	return (0);
 }
